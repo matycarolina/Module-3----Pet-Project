d62888457e748442c8b0d9f6a6c4990cd2355585
matycarolina <contrerasmarialejandra1@gamil.com>
2022-08-19 09:34:35 -0500

    Refactor test

--- Controller.js
+++ Controller.js
@@ -2,45 +2,63 @@ import axios from "axios";
 export const BASE_URL = "https://api.github.com/orgs/stackbuilders/repos";
 
 export const fill = async () => {
-  const response = await axios.get(BASE_URL);
-
-  const reposList = response.data;
-
-  return reposList;
+  try {
+    const response = await axios.get(BASE_URL);
+    const reposList = response.data;
+    return reposList;
+  } catch (error) {
+    return [];
+  }
 };
 
 export const formatList = (list) => {
   const formatedList = list.map((repo) => {
-    return {
-      name: repo.name,
-      stars: repo.stargazers_count,
-      updated: repo.updated_at,
-    };
+    try {
+      return {
+        name: repo.name,
+        stars: repo.stargazers_count,
+        updated: repo.updated_at,
+      };
+    } catch (error) {
+      return [];
+    }
   });
 
   return formatedList;
 };
 
 export const sumStars = (list) => {
-  const starsSum = list.reduce(
-    (previousValue, currentValue) =>
-      parseInt(previousValue + currentValue.stars),
-    0
-  );
+  try {
+    const starsSum = list.reduce(
+      (previousValue, currentValue) =>
+        parseInt(previousValue + currentValue.stars),
+      0
+    );
 
-  return starsSum;
+    return starsSum;
+  } catch (error) {
+    return 0;
+  }
 };
 
 export const filterByStars = (list) => {
-  const mostStarsRepos = list.filter((repo) => repo.stars > 5);
+  try {
+    const mostStarsRepos = list.filter((repo) => repo.stars > 5);
 
-  return mostStarsRepos;
+    return mostStarsRepos;
+  } catch (error) {
+    return [];
+  }
 };
 
 export const lastModified = (list) => {
-  const latestRepos = list
-    .sort((d1, d2) => new Date(d2.updated) - new Date(d1.updated))
-    .slice(0, 5);
+  try {
+    const latestRepos = list
+      .sort((d1, d2) => new Date(d2.updated) - new Date(d1.updated))
+      .slice(0, 5);
 
-  return latestRepos;
+    return latestRepos;
+  } catch (error) {
+    return [];
+  }
 };
