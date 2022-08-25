afb8980b4c90e7b9eb3e340e6de1f0264f333d23
matycarolina <contrerasmarialejandra1@gamil.com>
2022-08-25 09:40:53 -0500

    refactor code

--- Typescript/Controller.ts
+++ Typescript/Controller.ts
@@ -1,11 +1,16 @@
 import axios from "axios";
-import { OgRepo, testRepo } from "./test/dataTest";
 export const BASE_URL = "https://api.github.com/orgs/stackbuilders/repos";
 
 interface Repo {
   name: string;
   stars: number;
   updated: string;
+}
+
+interface OgRepo {
+  name: string;
+  stargazers_count: number;
+  updated_at: string;
 }
 
 export const fill = async () => {
@@ -18,54 +23,43 @@ export const fill = async () => {
   }
 };
 
-export const formatList = (list: OgRepo[] | testRepo[]): Repo[] => {
-  const formatedList = list.map((repo: any) => {
-    return <Repo>{
+export const formatList = (list: OgRepo[]) => {
+  return list.map((repo) => {
+    return {
       name: repo.name,
       stars: repo.stargazers_count,
       updated: repo.updated_at,
     };
   });
-  return formatedList;
 };
 
 export const sumStars = (list: Repo[]) => {
-  const starsSum = list.reduce(
+  return list.reduce(
     (previousValue: any, currentValue: Repo) =>
       parseInt(previousValue + currentValue.stars),
     0
   );
-
-  return starsSum;
 };
 
 export const filterByStars = (list: Repo[]) => {
-  const mostStarsRepos = list.filter((repo: Repo) => repo.stars > 5);
-
-  return mostStarsRepos;
+  return list.filter((repo: Repo) => repo.stars > 5);
 };
 
 export const lastModified = (list: Repo[]) => {
-  const latestRepos = list
+  return list
     .sort(
       (d1, d2) =>
         new Date(d2.updated).getTime() - new Date(d1.updated).getTime()
     )
     .slice(0, 5);
-
-  return latestRepos;
 };
 
 export const top5Stars = (list: Repo[]) => {
-  const top5Repos = list.sort((s1, s2) => s2.stars - s1.stars).slice(0, 5);
-
-  return top5Repos;
+  return list.sort((s1, s2) => s2.stars - s1.stars).slice(0, 5);
 };
 
 export const filterReposAlpha = (list: Repo[]) => {
-  const reposAlpha = list
-    .sort((n1, n2) => n1.name.localeCompare(n2.name))
-    .filter((repo: Repo) => repo.name[0] !== "h");
-
-  return reposAlpha;
+  return list
+    .filter((repo: Repo) => repo.name[0] !== "h")
+    .sort((n1, n2) => n1.name.localeCompare(n2.name));
 };
